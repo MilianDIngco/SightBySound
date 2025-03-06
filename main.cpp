3e6c42ccd6058936361e1b11d363aa10713b386d
MilianDIngco <mingco13@gmail.com>
2025-03-06 18:30:00 -0500

    Added period optimization
    This optimization likely won't improve performance as the period of the sum of sines we are using are ~> 1.28 seconds

--- main.cpp
+++ main.cpp
@@ -13,20 +13,27 @@
 #include <variant>
 #include "save_wav.h"
 #include <vector>
-#include <time.h>
+#include <time.h> // TODO: replace timing function to use chrono functions
+#include <chrono>
 #include <queue> // Image queue
 #include <semaphore.h>
 #include <mutex>
 #include <thread>
+#include <numeric>
 
 /*
 Naming Conventions
 variables are named using snake_case
 global variables are ALL_CAPS
 methods use camelCase
+
+TO USE PERIOD OPTIMIZATION, MAX FREQ AND MIN FREQ MUST BE WHOLE NUMBERS
+
+CHANGE MAX_HILBERT SIZE IF ORDER SIZE > 5
 */
 
 using ValueType = std::variant<int, double, std::string, bool>;
+using ChronoType = std::chrono::time_point<std::chrono::high_resolution_clock>;
 
 // -------------------------------- CHANGE THIS IF USING ORDER OF SIZE > 5
 constexpr size_t MAX_HILBERT_SIZE = 1024;
@@ -46,6 +53,8 @@ bool SAVE_IMG = 0;
 int CAM_1_INDEX = 0;
 int CAM_2_INDEX = 1;
 int N_RUNS = 1;
+bool PERIOD_OPTIMIZATION = false;
+bool DEBUG = false;
 
 struct Pair {
     int x;
@@ -157,7 +166,7 @@ void generateSines(short* samples, int sample_count, int n_pixel, int sample_rat
     float sample = 0;
     for (int n = 0; n < n_pixel; n++) {
         sample += std::sin(2.0f * M_PI * freqs[n] * static_cast<float>(i) / sample_rate) * volumes[n];
-    }
+    } 
     samples[i] = static_cast<short>((sample / n_pixel) * 32767 );
     // std::cout << samples[i] << std::endl;
   } 
@@ -169,11 +178,22 @@ void generateHilbert(struct Pair hilbert[], int n_pixels) {
   }
 }
 
-void generateFrequencies(float freqs[], double min_freq, double max_freq, int n_pixels) {
-  double freq_step = (max_freq - min_freq) / n_pixels;
+/** Return: A double representing the period of the sum of sines using these frequencies
+ */
+double generateFrequencies(float freqs[], double min_freq, double max_freq, int n_pixels) {
+  int lcm_num = n_pixels;
+  int gcd_den = min_freq * n_pixels;
   for (int i = 0; i < n_pixels; i++) {
-    freqs[i] = min_freq + (i * freq_step);
+    int numerator = i * (max_freq - min_freq) + min_freq * n_pixels;
+    int denominator = n_pixels;
+    freqs[i] = numerator / denominator; // I know its the wrong way around, its fine.
+    
+    // Period is 1 / f, so flip numerator and denominator
+    // T = lcm(d1, d2, ... dn) / gcd(n1, n2, ..., nn) | fi = ni / di
+    gcd_den = std::gcd(gcd_den, numerator);
   }
+
+  return (double) lcm_num / gcd_den;
 }
 
 void generateVolumes(float volumes[], cv::Mat image, struct Pair hilbert[], int n_pixels) {
@@ -228,6 +248,9 @@ void imageGen(cv::VideoCapture& cap, int image_width, std::queue<cv::Mat>& img_q
   const int MAX_IMAGES = 2;
 
   cv::Mat image;
+
+  std::cout << "image gen thread start" << std::endl;
+
   for (int i = 0; i < N_RUNS; i++) {
     // Capture image
     captureImage(image, cap, image_width);
@@ -246,7 +269,7 @@ void imageGen(cv::VideoCapture& cap, int image_width, std::queue<cv::Mat>& img_q
     sem_post(&img_sem);
   }
 
-
+  std::cout << "image gen thread end" << std::endl;
 }
 
 template <size_t N>
@@ -258,6 +281,8 @@ void audioGen(std::queue<cv::Mat>& img_queue, sem_t& img_sem, std::mutex& img_mu
   short* samples = nullptr;
   int sample_count = genSampleArray(samples, SAMPLE_RATE, DURATION);
   
+  std::cout << "audio gen thread start" << std::endl;
+
   for (int i = 0; i < N_RUNS; i++) {
     // Wait until an image is available in the queue
     sem_wait(&img_sem);
@@ -292,32 +317,47 @@ void audioGen(std::queue<cv::Mat>& img_queue, sem_t& img_sem, std::mutex& img_mu
     {
       std::lock_guard<std::mutex> lock_guard(audio_mutex);
       alSourceQueueBuffers(source, 1, &buffer);
-      std::cout << i << ": Pushed buffer onto queue" << std::endl;
+      // std::cout << i << ": Pushed buffer onto queue" << std::endl;
     }
     sem_post(&audio_sem);
   }
 
   delete[] samples;
 
+  std::cout << "audio gen thread end" << std::endl;
+
 }
 
 void audioPlay(sem_t& audio_sem, ALuint& source) {
 
+  std::cout << "audio play thread start" << std::endl;
+
   for (int i = 0; i < N_RUNS; i++) {
     sem_wait(&audio_sem);
-    
-    
+      
     ALint source_state;
     alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+    alSourcei(source, AL_LOOPING, ((PERIOD_OPTIMIZATION) ? AL_TRUE : AL_FALSE));
     if (source_state != AL_PLAYING) {
       // If the source isn't playing, start playback (or restart if needed)
       alSourcePlay(source);
     }
+
+    ChronoType start_time = std::chrono::high_resolution_clock::now();
     
     alGetSourcei(source, AL_SOURCE_STATE, &source_state);
     while (source_state == AL_PLAYING) {
-        alGetSourcei(source, AL_SOURCE_STATE, &source_state);
-        std::cout << "PLAYING" << std::endl;
+      if (PERIOD_OPTIMIZATION) {
+        ChronoType current_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = current_time - start_time;
+        if (elapsed.count() >= DURATION){
+          alSourcei(source, AL_LOOPING, AL_FALSE);
+          alSourceStop(source);
+          break;
+        }
+      }
+      alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+      // std::cout << "PLAYING" << std::endl;
     }
 
     // Pop processed audio from queue
@@ -328,11 +368,13 @@ void audioPlay(sem_t& audio_sem, ALuint& source) {
       ALuint processed_buffer;
       alSourceUnqueueBuffers(source, 1, &processed_buffer);
       alDeleteBuffers(1, &processed_buffer);
-      std::cout << i << ": Popped buffer from queue" << std::endl;
+      // std::cout << i << ": Popped buffer from queue" << std::endl;
       --processed;
     }
 
   }
+
+  std::cout << "audio play thread end" << std::endl;
 
 }
 
@@ -400,6 +442,7 @@ int main(int argc, char** argv) {
       getValue("CAM_1_INDEX", CAM_1_INDEX);
       getValue("CAM_2_INDEX", CAM_2_INDEX);
       getValue("N_RUNS", N_RUNS);
+      getValue("PERIOD_OPTIMIZATION", PERIOD_OPTIMIZATION);
 
     } else {
       std::cout << "Settings file failed to open\n Default settings applied" << std::endl;
@@ -421,7 +464,13 @@ int main(int argc, char** argv) {
     std::cout << "hilbert done" << std::endl;
   // ------------------------------------------------Generate frequency array------------------------------------------------
     float freqs[MAX_HILBERT_SIZE];
-    generateFrequencies(freqs, MIN_FREQ, MAX_FREQ, n_pixels);
+    double period = generateFrequencies(freqs, MIN_FREQ, MAX_FREQ, n_pixels);
+    if (PERIOD_OPTIMIZATION && period < DURATION) {
+      DURATION = period;
+      std::cout << "OPTIMIZATION: Using period optimization" << std::endl;
+    }
+    std::cout << "Period: " << period << std::endl;
+
     std::cout << "freq done" << std::endl;
 
   // ------------------------------------------------Sine waves------------------------------------------------
