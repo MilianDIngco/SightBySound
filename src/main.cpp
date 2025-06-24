a9b9b58bba5c9829e4952dddf0dc39436b592855
= <=>
2025-06-24 10:40:44 -0400

    Adjusted settings

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 using vec2D = std::vector<std::vector<int>>;
 using vec1D = std::vector<int>;
 int main(int argc, char** argv) {
-  std::string settings_filepath = "settings.txt";
+  std::string settings_filepath = "./settings/laptop_settings.txt";
   SightBySound sbs(settings_filepath);
   sbs.run();
   std::cout << "Finished" << std::endl;
