164fdb66d37a4e55ac48a0e2b4c8d6b0fdb4fa06
= <=>
2025-06-24 10:34:32 -0400

    Cleaned up project repo

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/milianingco/Research/SBS
-# Build directory: /home/milianingco/Research/SBS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("gtests")
--- build/gtests/sbstests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/milianingco/Research/SBS/build/gtests/sbstests[1]_tests.cmake")
-  include("/home/milianingco/Research/SBS/build/gtests/sbstests[1]_tests.cmake")
-else()
-  add_test(sbstests_NOT_BUILT sbstests_NOT_BUILT)
-endif()
